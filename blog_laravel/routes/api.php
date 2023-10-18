664ec3f43475ae2269f696598f22acc12faac1f4
Biswajit Pradhan <biswajit.pradhan@nettantra.net>
2023-10-18 21:29:20 +0530

    updaye

--- blog_laravel/routes/api.php
+++ blog_laravel/routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/test',function(){
-	return "God 353 ====>>> djjjj";
+	return "Test Biswajit";
 });
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
